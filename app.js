555ce1aff79c255d44898e028f0fdae4c0d67ffa
Kenan Casey <kenancasey@gmail.com>
2016-11-03 09:35:23 -0500

    basic clock

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-
 var main = function () {
     "use strict";
 
+    var useMilitaryTime = false;
+
+    setInterval(function () {
+        var currentDateTime = new Date();
+        var hour = currentDateTime.getHours();
+        var min = currentDateTime.getMinutes();
+
+        if (hour >= 12) {
+            var meridiem = "pm";
+        } else {
+            var meridiem = "am";
+        }
+
+        if (!useMilitaryTime) {
+            if (hour > 12) {
+                hour = hour - 12;
+            }
+        }
+
+        $("#time").text(hour + ":" + min + meridiem);
+
+    }, 500);
 }
 
 $(document).ready(main);
