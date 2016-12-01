7b92ed53f2baed01d9722a3e19be210906274f0d
Kenan Casey <kenancasey@gmail.com>
2016-12-01 09:51:05 -0600

    add basic lion alert functionality

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var main = function () {
     "use strict";
 
+    $("#lionAlert").hide();
+
     var useMilitaryTime = false;
 
     setInterval(function () {
@@ -31,6 +33,52 @@ var main = function () {
 
 
     }, 500);
+
+
+    function getLionAlerts() {
+        //get the rss feed
+
+        var rssURL = "http://www.getrave.com/rss/FHU/channel1";
+
+        $.ajax({
+            type: "GET",
+            url: rssURL,
+            dataType: "xml",
+            error: function () {
+                console.log("ERROR: Unable to load RSS feed. Check the URL and your connection status.");
+            },
+            success: function (xml) {
+
+                var $items = $(xml).find("item");
+
+                $items.each(function () {
+                    // extract the alert title
+                    var lionAlertTitle = $(this).find("title").text();
+                    console.log(lionAlertTitle);
+
+                    // extract the alert description 
+                    var lionAlertDescription = $(this).find("description").text();
+                    console.log(lionAlertDescription);
+
+                    var lionAlertDateString = $(this).find("pubDate").text();
+
+                    var lionAlertDate = Date(lionAlertDateString);
+
+                    // display title and description on page
+                    $("#lionAlert h1").html(lionAlertTitle);
+                    $("#lionAlert h2").text(lionAlertDescription);
+
+                    // show the alert 
+                    $("#lionAlert").slideDown(1000);
+
+                });
+
+            }
+        });
+
+    }
+
+    getLionAlerts();
 }
 
 $(document).ready(main);
